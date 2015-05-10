49e74718dbc1aabeaa6ec15759b5997bc2e1442a
Ivan Nem <agrocooker@gmail.com>
2015-05-11 00:24:04 +0300

    start

--- client.py
+++ client.py
@@ -1,10 +1,28 @@
-from http.client import HTTPConnection as Connect
+from http.client import HTTPConnection
+import json
 
-con = Connect('46.101.152.181:8080')
+server_addr = '46.101.152.181:8080'
 
-nick = input('nick=')
-header = {'Content-type': 'text/plain'}
-con.request('POST','',bytes(nick, 'utf-8'), header)
-con.close()
+def post_nick(nick='ivan'):
+    """
+    nick: str
+    post your nickname to remote server
+    """
+    con = HTTPConnection(server_addr)
+    header = {'Content-type': 'text/plain'}
+    con.request('POST','',bytes(nick, 'utf-8'), header)
+    con.close()
 
+def get_peers():
+    con = HTTPConnection(server_addr)
+    con.request('GET', '')
+    response = con.getresponse()
 
+    print(response.status)
+    d = json.loads(response.read().decode('utf-8'))
+    print(type(d))
+    return d
+
+if __name__ == '__main__':
+    post_nick()
+    print(get_peers())
